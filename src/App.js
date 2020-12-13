f979f73447932f58d9349c0b463027a8fa8e780e
kyawthura-gg <ggaryal@gmail.com>
2020-12-13 16:28:47 +0630

    added boostrap css and refactored

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+
+import Header from "./components/Header";
+import HomeScreen from "./pages/HomeScreen";
+import ProductScreen from "./pages/ProductScreen";
+import CartScreen from "./pages/CartScreen";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <>
+        <Header />
+        <main className="py-3">
+          <Container>
+            <Route path="/cart/:slug?" component={CartScreen} />
+            <Route path="/product/:slug" component={ProductScreen} />
+            <Route path="/" component={HomeScreen} exact />
+          </Container>
+        </main>
+      </>
+    </Router>
   );
 }
 
