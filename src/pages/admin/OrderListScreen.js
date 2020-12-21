2b961709c1b179d58ce7536fa2e32e8fddd0a136
kyawthura-gg <ggaryal@gmail.com>
2020-12-21 21:54:23 +0630

    Create OrderListScreen.js

---
+++ src/pages/admin/OrderListScreen.js
@@ -0,0 +1,83 @@
+import React, { useEffect } from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import { Table, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../../components/Message";
+import Loader from "../../components/Loader";
+import { listOrders } from "../../actions/orderActions";
+
+const OrderListScreen = ({ history }) => {
+  const dispatch = useDispatch();
+
+  const orderList = useSelector((state) => state.orderList);
+  const { loading, error, orders } = orderList;
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  useEffect(() => {
+    if (userInfo && userInfo.user.is_admin) {
+      dispatch(listOrders());
+    } else {
+      history.push("/login");
+    }
+  }, [dispatch, history, userInfo]);
+
+  return (
+    <>
+      <h1>orders</h1>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Table striped bordered hover responsive className="table-sm">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NAME</th>
+              <th>DATE</th>
+              <th>TOTAL</th>
+              <th>PAID</th>
+              <th>DELIVERED</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order) => (
+              <tr key={order.id}>
+                <td>{order.id}</td>
+                <td>{order.user && order.user.name}</td>
+                <td>{order.created_at.substring(0, 10)}</td>
+                <td>${order.total_price}</td>
+                <td>
+                  {order.is_paid ? (
+                    order.paid_at.substring(0, 10)
+                  ) : (
+                    <i className="fas fa-times" styles={{ color: "red" }}></i>
+                  )}
+                </td>
+                <td>
+                  {order.is_delivered ? (
+                    order.delivered_at.substring(0, 10)
+                  ) : (
+                    <i className="fas fa-times" styles={{ color: "red" }}></i>
+                  )}
+                </td>
+                <td>
+                  <LinkContainer to={`/order/${order.id}`}>
+                    <Button variant="light" className="btn-sm">
+                      Details
+                    </Button>
+                  </LinkContainer>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </>
+  );
+};
+
+export default OrderListScreen;
