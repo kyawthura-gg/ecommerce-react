b855d58f3f3960dbcadcf9fbd7e4e434a90e6b8b
kyawthura-gg <ggaryal@gmail.com>
2020-12-24 22:17:22 +0630

    changed colors

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -14,7 +14,7 @@ const SearchBox = ({ history }) => {
   };
 
   return (
-    <Form onSubmit={submitHandler} inline>
+    <Form onSubmit={submitHandler} inline className="m-auto">
       <Form.Control
         type="text"
         name="q"
@@ -22,7 +22,7 @@ const SearchBox = ({ history }) => {
         placeholder="Search ...."
         className="mr-sm-2 ml-sm-5"
       ></Form.Control>
-      <Button type="submit" variant="outline-success" className="p-2">
+      <Button type="submit" variant="outline-light" className="p-2">
         Search
       </Button>
     </Form>
